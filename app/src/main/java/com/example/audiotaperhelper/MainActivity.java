ec0c4782518f24476db5f81fa57cdfe7e18803b0
bananadan <glokback@gmail.com>
2022-01-29 16:06:04 +0300

    first commit

--- app/src/main/java/com/example/audiotaperhelper/MainActivity.java
+++ app/src/main/java/com/example/audiotaperhelper/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.audiotaperhelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.*;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private final static int REQUEST_ENABLE_BT = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetooth= BluetoothAdapter.getDefaultAdapter();
+        String status;
+        if(bluetooth!=null)
+        {
+            if (bluetooth.isEnabled()) {
+                // Bluetooth включен. Работаем.
+                String mydeviceaddress= bluetooth.getAddress();
+                String mydevicename= bluetooth.getName();
+                status= mydevicename+" : "+ mydeviceaddress;
+            }
+            else
+            {
+                // Bluetooth выключен. Предложим пользователю включить его.
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+//        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+
+
     }
 }
