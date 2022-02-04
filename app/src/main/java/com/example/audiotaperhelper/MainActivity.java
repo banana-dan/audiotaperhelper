9fc3aec042c9377fd55f79d60a8d5c67d1ebf269
banana-dan <79079738+banana-dan@users.noreply.github.com>
2022-02-04 21:55:52 +0300

    bluetooth added

--- app/src/main/java/com/example/audiotaperhelper/MainActivity.java
+++ app/src/main/java/com/example/audiotaperhelper/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.audiotaperhelper;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.*;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,24 +27,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BluetoothAdapter bluetooth= BluetoothAdapter.getDefaultAdapter();
-        String status;
+
         if(bluetooth!=null)
         {
+            ChooseDialog chooseDialog = new ChooseDialog();
+            ArrayList<String> devices = new ArrayList<String>();
+            String status;
             if (bluetooth.isEnabled()) {
-                // Bluetooth включен. Работаем.
-                String mydeviceaddress= bluetooth.getAddress();
-                String mydevicename= bluetooth.getName();
-                status= mydevicename+" : "+ mydeviceaddress;
+                Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                        devices.add(deviceName);
+                    }
+                }
+
             }
             else
             {
                 // Bluetooth выключен. Предложим пользователю включить его.
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                status="Bluetooth выключен";
             }
+
+
+            chooseDialog.setArrayRefVar(devices);
+            chooseDialog.show(getSupportFragmentManager(), "ffff");
         }
-//        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+
+
+
+
+
+
 
 
     }
+
+
+
 }
+
+
